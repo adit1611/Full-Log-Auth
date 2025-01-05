310f9b606b48baeb943479ccbb7db99f8ec195e8
adit1611 <aditya16112002@gmail.com>
2025-01-05 19:35:48 +0530

    Roleout

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
 	});
 }
 
